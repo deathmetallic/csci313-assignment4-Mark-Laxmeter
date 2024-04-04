17dd379c5fa524f9299e0bbb314405531464bc98
Mark Laxmeter <mslaxmeter@live.com>
2024-04-03 23:09:13 -0500

    Added login/registration forms, borrowed books list, related models.

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,7 +12,7 @@ admin.site.register(Language)
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
-    
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
@@ -32,6 +32,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -39,7 +40,7 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
 
