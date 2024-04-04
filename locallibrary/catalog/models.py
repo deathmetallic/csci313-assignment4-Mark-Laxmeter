ffdc2b32273ce507f280f462cf3f5203f6f0f7dd
Mark Laxmeter <mslaxmeter@live.com>
2024-04-03 22:21:34 -0500

    Added customized admin models, added language model.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -59,6 +59,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+
+    display_genre.short_description = 'Genre'
 
 
 import uuid # Required for unique book instances
@@ -112,3 +119,18 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            unique=True,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular language instance."""
+        return reverse('language-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
