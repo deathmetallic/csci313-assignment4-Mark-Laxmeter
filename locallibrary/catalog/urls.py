ebc5fda9391034efa6d45e3d0dd2dc9682b00e77
Mark Laxmeter <mslaxmeter@live.com>
2024-04-03 22:47:38 -0500

    Added book list page (all books), book detail page, added pagination

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
